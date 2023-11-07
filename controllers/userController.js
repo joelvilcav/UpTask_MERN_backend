410c62a78c670a49f30a0721687287814b8e753b
joelvilcav <jvilcav29@gmail.com>
2023-11-07 16:30:42 -0500

    Avoid duplicated email when creating users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,15 @@
 import User from '../models/User.js';
 
 const create = async (req, res) => {
+  // Avoid duplicated email
+  const { email } = req.body;
+  const userExisted = await User.findOne({ email });
+
+  if (userExisted) {
+    const error = new Error('User already exists');
+    res.status(400).send({ msg: error.message });
+  }
+
   try {
     const user = new User(req.body);
     const userSaved = await user.save();
