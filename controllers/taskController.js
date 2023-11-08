f49ccb4e31cbe4f8ca61c29039ab8df4f757174c
joelvilcav <jvilcav29@gmail.com>
2023-11-08 00:39:46 -0500

    Update task by user id authenticated

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -40,7 +40,32 @@ const getTask = async (req, res) => {
   res.json(task);
 };
 
-const updateTask = async (req, res) => {};
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const task = await Task.findById(id).populate('project');
+
+  if (!task) {
+    const error = new Error('Task not found');
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (task.project.owner.toString() !== req.user._id.toString()) {
+    const error = new Error('You are not allowed to add tasks');
+    return res.status(403).json({ msg: error.message });
+  }
+
+  task.name = req.body.name || task.name;
+  task.description = req.body.description || task.description;
+  task.priority = req.body.priority || task.priority;
+  task.deadline = req.body.deadline || task.deadline;
+
+  try {
+    const taskSaved = await task.save();
+    res.json(taskSaved);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const deleteTask = async (req, res) => {};
 
